6e20ceeb687c5d91cee0d0b885e3cc5748205635
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-09-08 23:01:31 -0300

    MAPS y Promises2

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,6 +5,7 @@ const Item = ({product}) => {
 
     const {id, img, name, description, price, stock}= product
     const navegar = useNavigate()
+    
   return (
     <div className="card" style={{width:'20rem', margin:'.5rem'}}>
     <img src={img} className="card-img-top" alt={name}/>
@@ -14,14 +15,9 @@ const Item = ({product}) => {
         <p className="card-text">${price}</p>
         <p className="card-text">stock: {stock}</p>
     </div>
-    {/* Navegacion con un boton  */}
+    
     <button className='btn btn-primary' onClick={()=>navegar(`/detalle/${id}`)}>Ver más</button>
-     {/* Navegacion con un link  */}
-    {/* <Link to={`/detalle/${id}`}>Ir al detalle</Link> */}
-        {/* Navegacion con boton y link (no recomiendo) */}
-        {/* <Link  to={`/detalle/${id}`}>
-          <button className='btn btn-primary'>Ver más</button> 
-        </Link> */}
+     
   </div>
 
   )
--- src/mocks/dataBase.js
+++ src/mocks/dataBase.js
@@ -1,21 +1,13 @@
-const productos = [
-    {   id:'01',
-       name:'random1',
-      category:'nuevos',
-       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis",
-      price:75,
-       img:'https://picsum.photos/200',
-        stock:5,
-       },
-    {id:'02', name:'random2', category:'nuevos', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:70, img:'https://picsum.photos/201',stock:15},
-    {id:'03', name:'random3', category:'nuevos', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:100, img:'https://picsum.photos/202',stock:7},
-    {id:'04', name:'random4', category:'oferta', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:75, img:'https://picsum.photos/203',stock:3},
-    {id:'05', name:'random5', category:'oferta', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:90, img:'https://picsum.photos/204',stock:10},
-    {id:'06', name:'random6', category:'oferta', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:50, img:'https://picsum.photos/205',stock:4},
-    {id:'07', name:'random7', category:'vendidos', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:50, img:'https://picsum.photos/205',stock:4},
-    {id:'08', name:'random8', category:'vendidos', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:50, img:'https://i.postimg.cc/qvwMmrH0/6mhmii.jpg',stock:4},
-    {id:'09', name:'random9', category:'vendidos', description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Bibendum at varius vel pharetra vel turpis nunc. Pellentesque dignissim enim sit amet venenatis", price:50, img:'https://picsum.photos/205',stock:4},
-  ]
+const productos = [{id:1,name:"Job",category:"Fire Sprinkler System",description:"Disp fx of base of nk of l femr, 7thG",price:8,img:'https://picsum.photos/201',stock:1},
+{id:2,name:"Sondra",category:"Drywall & Acoustical (MOB)",description:"Oth traumatic displ spondylolysis of fifth cervcal vertebra",price:76,img:'https://picsum.photos/202',stock:2},
+{id:3,name:"Marius",category:"Framing (Wood)",description:"Chronic gout due to renal impairment, unsp hand, with tophus",price:55,img:'https://picsum.photos/203',stock:3},
+{id:4,name:"Camala",category:"Termite Control",description:"Superficial foreign body, right great toe, initial encounter",price:57,img:'https://picsum.photos/204',stock:4},
+{id:5,name:"Gar",category:"Structural and Misc Steel (Fabrication)",description:"Dislocation of jaw, right side, subsequent encounter",price:45,img:'https://picsum.photos/205',stock:5},
+{id:6,name:"Mellisent",category:"Elevator",description:"Traum rupt of palmar ligmt of r idx fngr at MCP/IP jt, sqla",price:91,img:'https://picsum.photos/206',stock:6},
+{id:7,name:"Cody",category:"Painting & Vinyl Wall Covering",description:"External constriction, right ankle",price:29,img:'https://picsum.photos/207',stock:7},
+{id:8,name:"Darius",category:"Wall Protection",description:"Pathological fracture in other disease, pelvis",price:17,img:'https://picsum.photos/208',stock:8},
+{id:9,name:"Jenni",category:"Casework",description:"Type II occipital condyle fracture, unspecified side",price:54,img:'https://picsum.photos/209',stock:9},
+{id:10,name:"Oralle",category:"Retaining Wall and Brick Pavers",description:"Contact with nonvenomous frogs, subsequent encounter",price:49,img:'https://picsum.photos/201',stock:10}]
 
    export const data = new Promise((resolve, reject) => {
     //acciones
@@ -24,7 +16,7 @@ const productos = [
       if(condition){
         resolve(productos)
       }else{
-        reject('salio mal :( no hay sushi')
+        reject('Algo salio mal')
       }
 
     },3000)
